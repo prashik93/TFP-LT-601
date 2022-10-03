b23cade56677ab386e75925704aa8c99ca7988c8
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-03 14:32:35 +0530

    [prashik]operator's introduction

--- shellScript/shellScript.sh
+++ shellScript/shellScript.sh
@@ -11,3 +11,10 @@ read -p "Enter the commit id :- " commitId
 commitCount=`git log | grep -i commit | grep -v '\[' | awk -v var=$commitId '$2==var {print NR}'`
 echo $commitCount
 git rebase -i HEAD~$commitCount
+
+
+#Operators
+#1.Arithmetic operators:- (+, -, *, /, %, ++, --)
+#2.Relational operators:- (==, !=, <=, >=, <, >)
+#3.Boolean operators:- (&& or -a, || or -o)
+#4.File Test operators:-  
